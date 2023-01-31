620919386eba8e58da0e5da9d988864f7dd46d7c
Adham Mahgoub <adhammahgoub1@gmail.com>
2023-01-31 12:56:49 +0200

    upload

---
+++ Suspension/Final Version (Passive + Active)/Variables_Project.m
@@ -0,0 +1,6 @@
+ms  = 250 ;      %kg         sprung mass 
+mus = 50;        %kg         unsprung mass 
+ks  = 18600 ;    %N/m       spring
+cs  = 1000;      %N.s/m     damper
+kt  = 196000;    %N/m       tyre stiffness 
+
